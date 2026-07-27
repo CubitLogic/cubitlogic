cfd4fd88ca2d2df5745ebc270cb59dd96454e088
Bryon Hoyle <160192507+BryonHoyle@users.noreply.github.com>
2026-07-26 19:04:49 -0500

    Mount Qubit chat voice companion

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import NotFound from "@/pages/NotFound";
 import { Route, Switch } from "wouter";
 import ErrorBoundary from "./components/ErrorBoundary";
+import QubitChatCompanion from "./components/QubitChatCompanion";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import Home from "./pages/Home";
 import Blog from "./pages/Blog";
@@ -52,6 +53,7 @@ function App() {
         <TooltipProvider>
           <Toaster />
           <Router />
+          <QubitChatCompanion />
         </TooltipProvider>
       </ThemeProvider>
     </ErrorBoundary>
